be3683c3b471731342883d2451bd7274523bfcaa
Raime-34 <55358674+Raime-34@users.noreply.github.com>
2023-11-18 18:09:33 +0300

    аааааааааааааааааа

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
 /*
 === Утилита telnet ===
 
@@ -15,6 +24,39 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
 
-func main() {
+//"github.com/reiver/go-telnet"
 
+func main() {
+	myTelnet()
+}
+
+func myTelnet() error {
+	conn, err := net.DialTimeout("tcp", ":5555", 10*time.Second)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	go func() {
+		for {
+			msg, _ := bufio.NewReader(conn).ReadString('\n')
+			if msg == "" {
+				conn.Close()
+				os.Exit(0)
+			}
+			fmt.Println(msg)
+		}
+	}()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		line, _ := reader.ReadString('\n')
+		if strings.Contains(line, string([]byte{4})) {
+			conn.Close()
+			return nil
+		}
+		conn.Write([]byte(line))
+	}
 }
