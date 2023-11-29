c382d54961e434379b436dbfab063d7af984184f
Raime-34 <55358674+Raime-34@users.noreply.github.com>
2023-11-29 21:36:26 +0300

    fffff

--- develop/dev11/task_test.go
+++ develop/dev11/task_test.go
@@ -1,1 +1,22 @@
 package main
+
+import "testing"
+
+func Test_myTelnet(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "опять тестить",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := myTelnet(); (err != nil) != tt.wantErr {
+				t.Errorf("myTelnet() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
